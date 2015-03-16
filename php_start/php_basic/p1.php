358b3f1e0b1cb2cd23402fe77d06ede83007d391
dolare <dolarebrave@gmail.com>
2015-03-16 10:00:47 +0800

    p1 update

---
+++ php_start/php_basic/p1.php
@@ -0,0 +1,40 @@
+<b>aaaaaaaaaaaa</b>
+
+
+<html>
+<head>
+<title>
+<b>test</b>
+</title>
+<body>
+
+<?php 
+
+	echo 22222;
+
+
+	$a = 50;
+	echo "$a \n yuan";
+	var_dump($a);
+	$b = "abc123";
+	$c = (int)$b;
+	var_dump($c);
+	$d = "123abc";
+	var_dump((int)$d);
+	
+	define("NAME","dolare",true);
+	echo NAME;
+	
+
+?>
+
+</body>
+
+</head>
+
+
+
+
+
+
+</html>
